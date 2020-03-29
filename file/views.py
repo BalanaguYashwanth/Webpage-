8073dd7a4454ea08e6bbf6c74c7c72e6497e7467
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2020-03-29 18:39:17 +0530

    Add files via upload

--- file/views.py
+++ file/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render,redirect
 from .models import upload
+import zipfile,os, tempfile
+from django.forms import modelformset_factory
+from .forms import uploadForm
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response 
 
 # Create your views here.
 def home(request):
     if request.method=="POST":
         user=request.POST['user'];
         image=request.FILES['image'];
-        upload.objects.create(user=user,image=image);
+        upload.objects.create(user=user,image=image)
         files=upload.objects.all();
-        return redirect('foundation');
+        return redirect('foundation')
     else:
         return render(request,'image.html')
 
@@ -16,3 +22,9 @@ def foundation(request):
     files=upload.objects.all();
     return render(request,'foundation.html',{'files':files})
 
+
+class datalist(APIView):
+    def get(self,request):
+        datas=upload.objects.all()
+        serializer = uploadSerializers(datas,many=True)
+        return Response(serializer.data)
